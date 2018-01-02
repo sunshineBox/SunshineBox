0de777e6776b8e61a670b0302ab0d0563736dae1
shaolizhi1234 <shaolizhi1234@126.com>
2018-01-02 20:55:59 +0800

    完成了CourseActivity页面的UI

---
+++ app/src/main/java/shaolizhi/sunshinebox/ui/course/CourseActivity.java
@@ -0,0 +1,24 @@
+package shaolizhi.sunshinebox.ui.course;
+
+import android.os.Bundle;
+
+import shaolizhi.sunshinebox.R;
+import shaolizhi.sunshinebox.ui.base.BaseActivity;
+
+public class CourseActivity extends BaseActivity {
+
+    @Override
+    protected int layoutId() {
+        return R.layout.activity_course;
+    }
+
+    @Override
+    protected void created(Bundle bundle) {
+
+    }
+
+    @Override
+    protected void resumed() {
+
+    }
+}
